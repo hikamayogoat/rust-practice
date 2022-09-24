fa9cc3441b5d323100c4e9ce49a274438da20764
hikamayogoat <hikamayo.dw@gmail.com>
2022-09-24 22:16:30 +0900

    画面解像度に適応して座標を取得するようにする

--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -10,30 +10,28 @@ use opencv::{
 const ORIGINAL_FILENAME: &str = "tetris/original.jpg";
 const TET_FILENAME: &str = "tetris/sprint.jpg";
 
-// TODO: あとでよしなにやるけど一旦定数で持つ
-// memo: 検出がきびしそうなら画面サイズからの比率で出してもいけるかも
-
-const UPPER_LEFT: (usize, usize) = (615, 320);
-const UPPER_RIGHT: (usize, usize) = (1335, 320);
-const BOTTOM_LEFT: (usize, usize) = (615, 1760);
-const BOTTOM_RIGHT: (usize, usize) = (1335, 1760);
-
-// 比率版
-// const X_LOWER_RATIO: f32 = 6.2;
-// const X_UPPER_RATIO: f32 = 2.85;
-// const Y_LOWER_RATIO: f32 = 6.75;
-// const Y_UPPER_RATIO: f32 = 1.23;
-
-// 整数型で演算するので割り切れるように気をつける
+// マス数
 const CELL_WIDTH: usize = 10;
 const CELL_HEIGHT: usize = 20;
 
-// ネクストの座標: x_lower, x_upper, y_lower, y_upper
-const NEXT_1: (usize, usize, usize, usize) = (1400, 1650, 350, 500);
-const NEXT_2: (usize, usize, usize, usize) = (1400, 1600, 570, 680);
-const NEXT_3: (usize, usize, usize, usize) = (1400, 1600, 770, 890);
-const NEXT_4: (usize, usize, usize, usize) = (1400, 1600, 960, 1070);
-const NEXT_5: (usize, usize, usize, usize) = (1400, 1600, 1170, 1280);
+// 画面サイズをこの数値で割ると盤面の端の座標になる値。 (x_low, x_high, y_low, y_high)
+const FIELD_RATIO: (f32, f32, f32, f32) = (6.2, 2.87, 6.75, 1.22);
+
+// 画面サイズをこの数値で割るとネクストの領域の端の座標になる値。 (x_low, x_high, y_low, y_high)
+const NEXT1_POS_RASIO: (f32, f32, f32, f32) = (2.75, 2.34, 6.35, 4.4);
+const NEXT2_POS_RASIO: (f32, f32, f32, f32) = (2.75, 2.4, 3.85, 3.13);
+const NEXT3_POS_RASIO: (f32, f32, f32, f32) = (2.75, 2.4, 2.8, 2.45);
+const NEXT4_POS_RASIO: (f32, f32, f32, f32) = (2.75, 2.4, 2.25, 2.0);
+const NEXT5_POS_RASIO: (f32, f32, f32, f32) = (2.75, 2.4, 1.86, 1.68);
+
+fn ratio_to_position(display_width: u32, display_height: u32, ratio: (f32, f32, f32, f32)) -> (usize, usize, usize, usize) {
+    let lower_x = display_width as f32 / ratio.0;
+    let higher_x = display_width as f32 / ratio.1;
+    let lower_y = display_height as f32 / ratio.2;
+    let higher_y = display_height as f32 / ratio.3;
+
+    return (lower_x as usize, higher_x as usize, lower_y as usize, higher_y as usize);
+}
 
 // ミノの基準 HSV
 const S_MINO_HSV: (Mino, u16, u16, u16) =  (Mino::S, 100, 190, 180);
@@ -47,6 +45,8 @@ const HS_GAP: u16 = 10;
 const V_GAP: u16 = 50;
 
 #[derive(Debug)]
+#[derive(Clone)]
+#[derive(Copy)]
 enum Mino {
     S, Z, L, J, I, O, T, UNKNOWN
 }
@@ -64,16 +64,20 @@ fn read_image_file(filename: &str) -> Mat {
 }
 
 pub fn screen_test() {
+    let tmp_width = 1920u32;
+    let tmp_height = 1080u32;
+    let field_pos = ratio_to_position(tmp_width, tmp_height, FIELD_RATIO);
+
     // 画像読み込み
     let original_image = read_image_file(ORIGINAL_FILENAME);
-    let cliped_original = cut_field(&original_image);
+    let cliped_original = cut_rect(&original_image, field_pos);
 
     let image = read_image_file(TET_FILENAME);
-    let cliped_image = cut_field(&image);
+    let cliped_image = cut_rect(&image, field_pos);
 
     // 座標の区切りサイズを計算する
-    let width = UPPER_RIGHT.0 - UPPER_LEFT.0;
-    let height = BOTTOM_LEFT.1 - UPPER_LEFT.1;
+    let width = field_pos.1 - field_pos.0;
+    let height = field_pos.3 - field_pos.2;
     let cell_size = ((width / CELL_WIDTH), (height / CELL_HEIGHT));
 
     // 盤面を取得する
@@ -95,30 +99,18 @@ pub fn screen_test() {
     }
 
     // ネクストを取得する
-    let nexts = get_nexts(&image);
+    let next_ratios = [NEXT1_POS_RASIO, NEXT2_POS_RASIO, NEXT3_POS_RASIO, NEXT4_POS_RASIO, NEXT5_POS_RASIO];
+    let mut nexts = [Mino::UNKNOWN; 5];
+    for i in 0..next_ratios.len() {
+        let next_pos = ratio_to_position(tmp_width, tmp_height, next_ratios[i]);
+        let img = cut_rect(&image, next_pos);
+        nexts[i] = estimate_block(&img);
+    }
 
     println!("Nexts-------");
     println!("{:?}", nexts);
     imshow("test", &image);
 
-}
-
-// ネクストを取得する
-fn get_nexts(image: &Mat) -> [Mino; 5] {
-
-    let next1 = cut_rect(image, NEXT_1);
-    let next2 = cut_rect(image, NEXT_2);
-    let next3 = cut_rect(image, NEXT_3);
-    let next4 = cut_rect(image, NEXT_4);
-    let next5 = cut_rect(image, NEXT_5);
-
-    let n1 = estimate_block(&next1);
-    let n2 = estimate_block(&next2);
-    let n3 = estimate_block(&next3);
-    let n4 = estimate_block(&next4);
-    let n5 = estimate_block(&next5);
-
-    return [n1, n2, n3, n4, n5];
 }
 
 // ミノ判別する
@@ -196,28 +188,13 @@ fn estimate_block(image: &Mat) -> Mino {
     return Mino::UNKNOWN;
 }
 
-// 指定された領域を切り出す
+// 指定された領域を切り出して返す
 fn cut_rect(image: &Mat, pos: (usize, usize, usize, usize)) -> Mat {
-    let x = pos.0 as i32;
-    let y = pos.2 as i32;
-    let width = pos.1 as i32 - pos.0 as i32;
-    let height = pos.3 as i32 - pos.2 as i32;
-    
-    let rect = Mat::roi(
-        image,
-        Rect_{x: x, y: y, width: width, height: height}
-    ).unwrap();
-
-    return rect;
-}
-
-// 盤面部分だけ切り取る
-fn cut_field(image: &Mat) -> Mat {
-    let width = UPPER_RIGHT.0 - UPPER_LEFT.0;
-    let height = BOTTOM_LEFT.1 - UPPER_LEFT.1;
+    let width = pos.1 - pos.0;
+    let height = pos.3 - pos.2;
     let cliped = Mat::roi(
         &image,
-        Rect_ { x: (UPPER_LEFT.0) as i32, y: (UPPER_LEFT.1) as i32, width: (width) as i32, height: (height) as i32 }
+        Rect_ { x: (pos.0) as i32, y: (pos.2) as i32, width: (width) as i32, height: (height) as i32 }
     ).unwrap();
     return cliped;
 }
@@ -249,8 +226,6 @@ fn get_field_info(original: &Mat, image: &Mat, cell_size: &(usize, usize)) -> [[
             let cell_data = cell_c.data_typed::<Vec3b>().unwrap();
             let mean_hsv = mean_vec(cell_data);
 
-            println!("{:?}", mean_hsv);
-
             if mean_hsv.2 >= 100 {
                 field[x][y] = Field::EXIST;
             } else {
@@ -277,16 +252,16 @@ fn mean_vec(vec: &[Vec3b]) -> (u8, u8, u8) {
     return (h as u8, s as u8, v as u8);
 }
 
-fn get_ss() -> Mat {
+fn get_ss(screen_number: usize) -> (Mat, u32, u32) {
 
     let screens = Screen::all();
-    let main_screen = screens.unwrap()[1];
-    let capture = main_screen.capture().unwrap();
+    let target_screen = screens.unwrap()[screen_number];
+    let capture = target_screen.capture().unwrap();
     let buffer = capture.buffer();
 
     let mat = imdecode(&VectorOfu8::from_iter(buffer.clone()), 1).unwrap();
 
-    return mat;
+    return (mat, capture.width(), capture.height());
 }
 
 fn imshow(name: &str, image: &Mat) {
