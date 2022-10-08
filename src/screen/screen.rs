f971951c0538f8d94e3a1eb9f1637d11f869e773
hikamayogoat <hikamayo.dw@gmail.com>
2022-10-08 17:45:35 +0900

    できるだけ構造体にまとめる

--- src/screen/screen.rs
+++ src/screen/screen.rs
@@ -8,47 +8,72 @@ use opencv::{
 use crate::screen::experimental::*;
 use crate::screen::values::*;
 
+type FieldMatrix = [[CellState; CELL_HEIGHT]; CELL_WIDTH];
+pub struct BoardState {
+    field: FieldMatrix,
+    nexts: [Mino; 5]
+}
+
+struct ScreenShot {
+    image: Mat,
+    width: u32,
+    height: u32
+}
+
+struct RectPosition {
+    lower_x: usize,
+    upper_x: usize,
+    lower_y: usize,
+    upper_y: usize
+}
+
 pub fn get_board_state(screen_number: usize) -> BoardState {
     // 画面取得
     let ss = get_ss(screen_number);
-    let image = ss.0;
-    let display_width = ss.1;
-    let display_height = ss.2;
-    let field_pos = ratio_to_position(display_width, display_height, FIELD_RATIO);
-    let cliped_image = cut_rect(&image, field_pos);
+    let field_pos = ratio_to_position(ss.width, ss.height, &FIELD_RATIO);
+    let cliped_image = cut_rect(&ss.image, &field_pos);
 
     // 画像読み込み
     let original_image = read_image_file(ORIGINAL_FILENAME);
-    let cliped_original = cut_rect(&original_image, field_pos);
-
+    let cliped_original = cut_rect(&original_image, &field_pos);
 
     // 座標の区切りサイズを計算する
-    let width = field_pos.1 - field_pos.0;
-    let height = field_pos.3 - field_pos.2;
+    let width = field_pos.upper_x - field_pos.lower_x;
+    let height = field_pos.upper_y - field_pos.lower_y;
     let cell_size = ((width / CELL_WIDTH), (height / CELL_HEIGHT));
 
     // 盤面を取得する
-    let mut field = [[CellState::UNKNOWN; CELL_HEIGHT]; CELL_WIDTH];
-    field = get_field_info(&cliped_original, &cliped_image, &cell_size);
+    let field = get_field_state(&cliped_original, &cliped_image, &cell_size);
 
     // ネクストを取得する
-    let next_ratios = [NEXT1_POS_RASIO, NEXT2_POS_RASIO, NEXT3_POS_RASIO, NEXT4_POS_RASIO, NEXT5_POS_RASIO];
     let mut nexts = [Mino::UNKNOWN; 5];
-    for i in 0..next_ratios.len() {
-        let next_pos = ratio_to_position(display_width, display_height, next_ratios[i]);
-        let img = cut_rect(&image, next_pos);
+    for i in 0..nexts.len() {
+        let next_pos = ratio_to_position(ss.width, ss.height, &NEXT_RATIOS[i]);
+        let img = cut_rect(&ss.image, &next_pos);
         nexts[i] = estimate_block(&img);
     }
 
     return BoardState{ field: field, nexts: nexts };
 }
 
+fn ratio_to_position(display_width: u32, display_height: u32, ratio: &PositionRatio) -> RectPosition {
+    let lower_x = display_width as f32 / ratio.lower_x_ratio;
+    let upper_x = display_width as f32 / ratio.upper_x_ratio;
+    let lower_y = display_height as f32 / ratio.lower_y_ratio;
+    let upper_y = display_height as f32 / ratio.upper_y_ratio;
+
+    return RectPosition {
+        lower_x: lower_x as usize, 
+        upper_x: upper_x as usize, 
+        lower_y: lower_y as usize, 
+        upper_y: upper_y as usize
+    };
+}
 
 // ミノ判別する
 // 正直 kmeans でわざわざやるほどでもない気はするが一旦このまま
 fn estimate_block(image: &Mat) -> Mino {
     const CLUSTER_NUM: i32 = 3;
-    let mino_hsv_vec = vec![S_MINO_HSV, Z_MINO_HSV, L_MINO_HSV, J_MINO_HSV, I_MINO_HSV, T_MINO_HSV, O_MINO_HSV];
 
     // HSVに変換する
     let mut image_hsv = Mat::default();
@@ -82,35 +107,33 @@ fn estimate_block(image: &Mat) -> Mino {
     let mut center_u = Mat::default();
     center.convert_to(&mut center_u, CV_16UC3, 1.0, 0.0);
 
+    // 候補値が3要素区切りで直列に並んだ配列 (h,s,v,h,s,v,h...)
     let cands_serial = center_u.data_typed::<u16>().unwrap();
 
     for i in 0..CLUSTER_NUM {
         let first = (i * 3) as usize;
-        let candidate: [u16; 3] = [cands_serial[first]*2, cands_serial[first+1], cands_serial[first+2]];
+        let candidate: MinoHSV  = MinoHSV {
+            kind: Mino::UNKNOWN,
+            h: cands_serial[first]*2, // H値を0〜360にスケールする
+            s: cands_serial[first+1], 
+            v: cands_serial[first+2]
+        };
 
-        // 黒は切り捨てておく
-        if candidate[2] <= 100 {
+        // 黒っぽかったら切り捨てておく
+        if candidate.v <= 100 {
             continue;
         }
 
-        for mino in &mino_hsv_vec {
+        for mino in MINO_HSV_LIST{
             // u16だと負の値になるかもしれないので対策しておく
-            let h_range = (std::cmp::max(HS_GAP, mino.1) - HS_GAP, mino.1 + HS_GAP);
-            let s_range = (std::cmp::max(V_GAP, mino.2) - V_GAP, mino.2 + V_GAP);
-            let v_range = (std::cmp::max(V_GAP, mino.3) - V_GAP, mino.3 + V_GAP);
-            if candidate[0] >= h_range.0 && candidate[0] <= h_range.1 {
-                if candidate[1] >= s_range.0 && candidate[1] <= s_range.1 {
-                    if candidate[2] >= v_range.0 && candidate[2] <= v_range.1 {
-                        match mino.0 {
-                            Mino::S => return Mino::S,
-                            Mino::Z => return Mino::Z,
-                            Mino::L => return Mino::L,
-                            Mino::J => return Mino::J,
-                            Mino::I => return Mino::I,
-                            Mino::T => return Mino::T,
-                            Mino::O => return Mino::O,
-                            _ => {}
-                        }
+            let h_range = (std::cmp::max(ALLOW_H_GAP, mino.h) - ALLOW_H_GAP, mino.h + ALLOW_H_GAP);
+            let s_range = (std::cmp::max(ALLOW_S_GAP, mino.s) - ALLOW_S_GAP, mino.s + ALLOW_S_GAP);
+            let v_range = (std::cmp::max(ALLOW_V_GAP, mino.v) - ALLOW_V_GAP, mino.v + ALLOW_V_GAP);
+
+            if candidate.h >= h_range.0 && candidate.h <= h_range.1 {
+                if candidate.s >= s_range.0 && candidate.s <= s_range.1 {
+                    if candidate.v >= v_range.0 && candidate.v <= v_range.1 {
+                        return mino.kind
                     }
                 }
             }
@@ -120,18 +143,18 @@ fn estimate_block(image: &Mat) -> Mino {
 }
 
 // 指定された領域を切り出して返す
-fn cut_rect(image: &Mat, pos: (usize, usize, usize, usize)) -> Mat {
-    let width = pos.1 - pos.0;
-    let height = pos.3 - pos.2;
+fn cut_rect(image: &Mat, pos: &RectPosition) -> Mat {
+    let width = pos.upper_x - pos.lower_x;
+    let height = pos.upper_y - pos.lower_y;
     let cliped = Mat::roi(
         &image,
-        Rect_ { x: (pos.0) as i32, y: (pos.2) as i32, width: (width) as i32, height: (height) as i32 }
+        Rect_ { x: (pos.lower_x) as i32, y: (pos.lower_y) as i32, width: (width) as i32, height: (height) as i32 }
     ).unwrap();
     return cliped;
 }
 
-fn get_field_info(original: &Mat, image: &Mat, cell_size: &(usize, usize)) -> [[CellState; CELL_HEIGHT]; CELL_WIDTH] {
-    let mut field = [[CellState::UNKNOWN; CELL_HEIGHT]; CELL_WIDTH];
+fn get_field_state(original: &Mat, image: &Mat, cell_size: &(usize, usize)) -> FieldMatrix {
+    let mut field: FieldMatrix = [[CellState::UNKNOWN; CELL_HEIGHT]; CELL_WIDTH];
 
     // 背景差分を計算する
     let mut diff_rgb = Mat::default();
@@ -183,7 +206,7 @@ fn mean_vec(vec: &[Vec3b]) -> (u8, u8, u8) {
     return (h as u8, s as u8, v as u8);
 }
 
-fn get_ss(screen_number: usize) -> (Mat, u32, u32) {
+fn get_ss(screen_number: usize) -> ScreenShot {
 
     let screens = Screen::all();
     let target_screen = screens.unwrap()[screen_number];
@@ -192,5 +215,9 @@ fn get_ss(screen_number: usize) -> (Mat, u32, u32) {
 
     let mat = imdecode(&VectorOfu8::from_iter(buffer.clone()), 1).unwrap();
 
-    return (mat, capture.width(), capture.height());
+    return ScreenShot {
+        image: mat, 
+        width: capture.width(), 
+        height: capture.height()
+    };
 }
